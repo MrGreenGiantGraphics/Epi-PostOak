97dee01d1f46559e72f28695a30b3e11d2d0ab30
GreenGiantGraphic_cp <SND\GreenGiantGraphic_cp>
2016-05-25 18:00:19 +0000

    resort

---
+++ Landrys.ThePostOak_EpiServer/Landrys.ThePostOak_EpiServer/Models/Pages/ResortPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace Landrys.ThePostOak_EpiServer.Models.Pages
+{
+    [ContentType(
+        DisplayName = "ResortPage", 
+        GUID = "aa1f2ba8-884d-44b7-a1bb-71dd72c78ce0", 
+        Description = "")]
+    public class ResortPage : PageData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
